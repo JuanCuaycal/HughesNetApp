339a501ab5018228334a1db15648d8047e6a136e
Juan Cuaycal <juancuaycal@gmail.com>
2020-12-30 17:25:26 -0500

    Reclutado

---
+++ app/src/main/java/com/hughesnet/hughesnetapp/Reclutado.java
@@ -0,0 +1,14 @@
+package com.hughesnet.hughesnetapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Reclutado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reclutado);
+    }
+}
