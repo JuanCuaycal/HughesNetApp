097f340aaeb4a23c3d0f3123bfa7bcffc6599abc
Juan Cuaycal <juancuaycal@gmail.com>
2020-12-30 12:40:15 -0500

    Tarjetero

--- app/src/main/java/com/hughesnet/hughesnetapp/MainActivity.java
+++ app/src/main/java/com/hughesnet/hughesnetapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.hughesnet.hughesnetapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button bt1=findViewById(R.id.btn_loggin);
+        Button bt2=findViewById(R.id.btn_registro);
+
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(view.getContext(),Actividades.class);
+                startActivityForResult(intent,0);
+            }
+        });
+
+
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v2) {
+                Intent intent=new Intent(v2.getContext(),Formulario.class);
+                startActivityForResult(intent,0);
+            }
+        });
+
+
+
     }
+
+
+
 }
